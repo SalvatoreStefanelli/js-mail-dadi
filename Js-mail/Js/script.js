4be3d89ff8eff5b99bf9ddfce63492f96e91c7a9
SalvatoreStefanelli <stefanellisalvatore.ss@gmail.com>
2023-12-05 22:21:16 +0100

    add condition to ex

--- Js-mail/Js/script.js
+++ Js-mail/Js/script.js
@@ -4,22 +4,22 @@
 const email = ['Naruto@gmail.com', 'Levi@libero.it', 'RoronoaZoro@hotmail.it'];
 
 // Chiedo all'utente la sua mail con il prompt e lo salvo in una variabile
-//const user_mail = prompt('Enter your email');
+const user_mail = prompt('Enter your email');
 //console.log(user_mail);
 
 // Creo un ciclo per elencare la lista
 for (let i = 0; i < email.length; i++) {
     const element = email[i];
-    console.log(element); 
+    //console.log(element); 
     
     // Se la mail dell'utente è presente nel ciclo, stampo il messaggio 'Access Allowed'
-    // if (user_mail == i) {
-    //     console.log('Access Allowed');
+     if (user_mail == element) {
+         console.log('Access Allowed');
 
-    //     // Altrimenti stampo il messaggio 'Access Denied' 
-    // } else {
-    //     console.log('Access Denied');
-    // }
+         // Altrimenti stampo il messaggio 'Access Denied' 
+     } else {
+         console.log('Access Denied');
+     }
 }
 
 
